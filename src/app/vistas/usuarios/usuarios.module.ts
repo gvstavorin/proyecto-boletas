deb53faa608c64fbefc00570c96def3cd845fddd
gustavo <gustavoandreescg@gmail.com>
2019-05-20 19:00:58 -0400

    commit

--- src/app/vistas/usuarios/usuarios.module.ts
+++ src/app/vistas/usuarios/usuarios.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {DatatableComponent} from '../usuarios/datatable/datatable.component';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { UsuariosComponent } from './usuarios.component';
 import { PageHeaderModule } from '../Componentes/page-header/page-header.module';
+import {RegistroUsuariosComponent} from '../usuarios/registro-usuarios/registro-usuarios.component';
 
 @NgModule({
-  declarations: [UsuariosComponent],
+  declarations: [UsuariosComponent,DatatableComponent,RegistroUsuariosComponent],
   imports: [
     CommonModule,
     UsuariosRoutingModule,
